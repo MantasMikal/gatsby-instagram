0aaaad5e4e684228a3fb151d5d85e3733ccd306e
Mantas Mikalauskas <mantas.codes@gmail.com>
2022-05-20 18:20:56 +0300

    chore: adds a comment to store token in a .env

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,13 +13,15 @@ module.exports = {
         background_color: '#fff',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/gatsby-icon.png',
       },
     },
     {
       // resolve: require.resolve(`./plugins/gatsby-source-instagram-all`),
       resolve: `gatsby-source-instagram-all`,
       options: {
+        // don't forget to store your access token in a .env file
+        // https://www.gatsbyjs.com/docs/how-to/local-development/environment-variables/
         access_token: "IGQVJVWFY2Tkw3SlBzaFJOZAzc3M1dqVU14SkM1UHZA4UVZAzM284ckNHaVBRZAmtYZA2hyaXpkbzcyOVV1LWN1dk5rX19SaHJVc3hmamVCN3FCQXhRQmtfMUxjNDhIdlJibFRleFNxaFRtVUF1eGhFYnFqbQZDZD"
       }
     },
